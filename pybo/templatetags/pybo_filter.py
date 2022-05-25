a362f3c9667237b1cfe4710fb123eabdca7c218a
박지훈 <qkrmfqkrmf97@gmail.com>
2022-05-25 10:54:21 +0900

    search

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -4,9 +4,11 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.filter
+@register.filter()
 def mark(value):
     extensions = ["nl2br", "fenced_code"]
     return mark_safe(markdown.markdown(value, extensions=extensions))
+
+@register.filter
 def sub(value, arg):
     return value - arg
